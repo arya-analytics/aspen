f92582d273e9676ee6f80039e20c1d7f1743a026
emiliano bonilla <emilbon99@gmail.com>
2022-05-23 16:35:47 -0700

    DA-156 added gossipOnceWith

--- internal/cluster/gossip/gossip.go
+++ internal/cluster/gossip/gossip.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/arya-analytics/aspen/internal/cluster/store"
 	"github.com/arya-analytics/aspen/internal/node"
+	"github.com/arya-analytics/x/address"
 	"github.com/arya-analytics/x/rand"
 	"github.com/arya-analytics/x/shutdown"
 	"github.com/arya-analytics/x/transport"
@@ -51,32 +52,27 @@ func (g *Gossip) GossipOnce(ctx context.Context) error {
 	host := g.store.Host()
 	host.Heartbeat = host.Heartbeat.Increment()
 	g.store.Set(host)
-
-	var (
-		snap = g.store.GetState()
-		peer = randomPeer(snap)
-		sync = Message{Digests: snap.Nodes.Digests()}
-	)
+	peer := randomPeer(g.store.GetState())
 	if peer.Address == "" {
 		g.Logger.Warn("no healthy nodes to gossip with")
 		return nil
 	}
-
 	g.Logger.Debug("initiating gossip",
 		zap.Uint32("initiator", uint32(host.ID)),
 		zap.Uint32("peer", uint32(peer.ID)),
 		zap.Uint32("version", host.Heartbeat.Version),
 	)
+	return g.GossipOnceWith(ctx, peer.Address)
 
-	ack, err := g.Transport.Send(ctx, peer.Address, sync)
+}
+
+func (g *Gossip) GossipOnceWith(ctx context.Context, addr address.Address) error {
+	sync := Message{Digests: g.store.GetState().Nodes.Digests()}
+	ack, err := g.Transport.Send(ctx, addr, sync)
 	if err != nil {
 		return err
 	}
-	ack2, err := g.ack(ack)
-	if err != nil {
-		return err
-	}
-	_, err = g.Transport.Send(ctx, peer.Address, ack2)
+	_, err = g.Transport.Send(ctx, addr, g.ack(ack))
 	return err
 }
 
@@ -87,18 +83,19 @@ func (g *Gossip) processGossip(ctx context.Context, msg Message) (Message, error
 
 	switch msg.variant() {
 	case messageVariantSync:
-		return g.sync(msg)
+		return g.sync(msg), nil
 	case messageVariantAck:
-		return g.ack(msg)
+		return g.ack(msg), nil
 	case messageVariantAck2:
-		return g.ack2(msg)
+		g.ack2(msg)
+		return Message{}, nil
 	default:
 		g.Logger.Panic("gossip received empty message")
 		return Message{}, nil
 	}
 }
 
-func (g *Gossip) sync(sync Message) (ack Message, err error) {
+func (g *Gossip) sync(sync Message) (ack Message) {
 	snap := g.store.GetState()
 	ack = Message{Nodes: make(node.Group), Digests: make(node.Digests)}
 	for _, dig := range sync.Digests {
@@ -126,10 +123,10 @@ func (g *Gossip) sync(sync Message) (ack Message, err error) {
 		}
 	}
 
-	return ack, nil
+	return ack
 }
 
-func (g *Gossip) ack(ack Message) (ack2 Message, err error) {
+func (g *Gossip) ack(ack Message) (ack2 Message) {
 	// Take a snapshot before we merge the peer's nodes.
 	snap := g.store.GetState()
 	g.store.Merge(ack.Nodes)
@@ -141,12 +138,11 @@ func (g *Gossip) ack(ack Message) (ack2 Message, err error) {
 			ack2.Nodes[dig.ID] = n
 		}
 	}
-	return ack2, nil
+	return ack2
 }
 
-func (g *Gossip) ack2(ack2 Message) (msg Message, err error) {
+func (g *Gossip) ack2(ack2 Message) {
 	g.store.Merge(ack2.Nodes)
-	return msg, err
 }
 
 func randomPeer(snap store.State) node.Node {
