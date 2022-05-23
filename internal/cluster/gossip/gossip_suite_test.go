1fbbacb21a05aacfa2d185d6b7ce274008ff367c
emiliano bonilla <emilbon99@gmail.com>
2022-05-23 16:51:32 -0700

    DA-156 added interval test

--- internal/cluster/gossip/gossip_suite_test.go
+++ internal/cluster/gossip/gossip_suite_test.go
@@ -1,10 +1,15 @@
 package gossip_test
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+)
+
+var (
+	ctx = context.Background()
 )
 
 func TestGossip(t *testing.T) {
--- internal/cluster/gossip/gossip_test.go
+++ internal/cluster/gossip/gossip_test.go
@@ -1,13 +1,14 @@
 package gossip_test
 
 import (
-	"context"
 	"github.com/arya-analytics/aspen/internal/cluster/store"
 	"github.com/arya-analytics/aspen/internal/node"
+	shut "github.com/arya-analytics/x/shutdown"
 	tmock "github.com/arya-analytics/x/transport/mock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"go.uber.org/zap"
+	"time"
 
 	"github.com/arya-analytics/aspen/internal/cluster/gossip"
 )
@@ -22,24 +23,41 @@ var _ = Describe("Gossip", func() {
 		logger = zap.NewNop()
 	})
 	Describe("Two Node", func() {
-		It("Should converge after a single exchange", func() {
-			t1, t2, t3 := net.Route(""), net.Route(""), net.Route("")
-			nodes := node.Group{1: {ID: 1, Address: t1.Address}, 2: {ID: 2, Address: t2.Address}}
-			sOne := store.New()
+		var (
+			t1, t2, t3      *tmock.Unary[gossip.Message, gossip.Message]
+			nodes, nodesTwo node.Group
+			sOne            store.Store
+			g1              *gossip.Gossip
+			sd              shut.Shutdown
+		)
+		BeforeEach(func() {
+			t1, t2, t3 = net.Route(""), net.Route(""), net.Route("")
+			nodes = node.Group{1: {ID: 1, Address: t1.Address}, 2: {ID: 2, Address: t2.Address}}
+			sOne = store.New()
 			sOne.SetState(store.State{Nodes: nodes, HostID: 1})
-			nodesTwo := nodes.Copy()
+			nodesTwo = nodes.Copy()
 			nodesTwo[3] = node.Node{ID: 3, Address: t3.Address, State: node.StateDead}
 			sTwo := store.New()
 			sTwo.SetState(store.State{Nodes: nodesTwo, HostID: 2})
-			g1 := gossip.New(sOne, gossip.Config{Transport: t1, Logger: logger})
-			gossip.New(sTwo, gossip.Config{Transport: t2, Logger: logger})
-			ctx := context.Background()
+			sd = shut.New()
+			g1 = gossip.New(sOne, gossip.Config{Transport: t1, Logger: logger, Shutdown: sd, Interval: 5 * time.Millisecond})
+			gossip.New(sTwo, gossip.Config{Transport: t2, Logger: logger, Shutdown: sd, Interval: 5 * time.Millisecond})
+		})
+		It("Should converge after a single exchange", func() {
 			Expect(g1.GossipOnce(ctx)).To(Succeed())
 			Expect(sOne.GetState().Nodes).To(HaveLen(3))
 			Expect(sOne.GetState().Nodes[1].Heartbeat.Version).To(Equal(uint32(1)))
 			Expect(sOne.GetState().Nodes[3].State).To(Equal(node.StateDead))
 			Expect(sOne.GetState().Nodes[2].Heartbeat.Version).To(Equal(uint32(0)))
 		})
+		It("Should gossip at the correct interval", func() {
+			g1.Gossip(ctx)
+			time.Sleep(12 * time.Millisecond)
+			Expect(sd.Shutdown()).To(Succeed())
+			Expect(sOne.GetState().Nodes).To(HaveLen(3))
+			Expect(sOne.GetState().Nodes[1].Heartbeat.Version).To(Equal(uint32(2)))
+			Expect(sOne.GetState().Nodes[3].State).To(Equal(node.StateDead))
+			Expect(sOne.GetState().Nodes[2].Heartbeat.Version).To(Equal(uint32(0)))
+		})
 	})
-
 })
