5ce5aa4eec9324d3965b426be5ad978ae5c9f11b
emiliano bonilla <emilbon99@gmail.com>
2022-05-24 13:23:36 -0700

    DA-156added store tests and wrote docs'

--- internal/cluster/store/store.go
+++ internal/cluster/store/store.go
@@ -1,3 +1,5 @@
+// Package store exposes a simple copy-on-read store for managing cluster state.
+// To create a new store, call store.New().
 package store
 
 import (
@@ -8,40 +10,54 @@ import (
 	"io"
 )
 
+// Store is an interface representing a copy-on-read store for managing cluster state.
 type Store interface {
+	// Observable allows the caller to react to state changes. This state is not diffed i.e.
+	// any call that modifies the state, even if no actual change occurs, will get sent to the
+	// Observable.
 	store.Observable[State]
+	// FlushLoader allows the caller to flush and load State from a persistent store.
 	kv.FlushLoader
+	// Set sets a node in state.
 	Set(node.Node)
+	// Get returns a node from state. Returns false if the node is not found.
 	Get(node.ID) (node.Node, bool)
+	// Merge merges a node.Group into State.Nodes by selecting nodes from group with heartbeats
+	// that are either not in State or are older than in State.
 	Merge(group node.Group)
+	// Valid returns true if the store is valid i.e. State.HostID has been set.
 	Valid() bool
-	Host() node.Node
+	// GetHost returns the host node of the store.
+	GetHost() node.Node
+	// SetHost sets the host for the store.
 	SetHost(node.Node)
 }
 
 func _copy(s State) State { return State{Nodes: s.Nodes.Copy(), HostID: s.HostID} }
 
+// New opens a new empty, invalid Store.
 func New() Store {
 	c := &core{Observable: store.ObservableWrap[State](store.New(_copy))}
 	c.Observable.SetState(State{Nodes: make(node.Group)})
 	return c
 }
 
+// State is the current state of the cluster as viewed from the host.
 type State struct {
 	Nodes  node.Group
 	HostID node.ID
 }
 
-type core struct {
-	store.Observable[State]
-}
+type core struct{ store.Observable[State] }
 
+// Get implements Store.
 func (c *core) Get(id node.ID) (node.Node, bool) {
 	n, ok := c.Observable.GetState().Nodes[id]
 	return n, ok
 }
 
-func (c *core) Host() node.Node {
+// Host implements Store.
+func (c *core) GetHost() node.Node {
 	snap := c.Observable.GetState()
 	if n, ok := snap.Nodes[snap.HostID]; ok {
 		return n
@@ -49,6 +65,7 @@ func (c *core) Host() node.Node {
 	panic("no host")
 }
 
+// SetHost implements Store.
 func (c *core) SetHost(n node.Node) {
 	snap := c.Observable.GetState()
 	snap.Nodes[n.ID] = n
@@ -56,12 +73,14 @@ func (c *core) SetHost(n node.Node) {
 	c.Observable.SetState(snap)
 }
 
+// Set implements Store.
 func (c *core) Set(n node.Node) {
 	snap := c.Observable.GetState()
 	snap.Nodes[n.ID] = n
 	c.Observable.SetState(snap)
 }
 
+// Merge implements Store.
 func (c *core) Merge(other node.Group) {
 	snap := c.Observable.GetState()
 	for _, n := range other {
@@ -73,10 +92,10 @@ func (c *core) Merge(other node.Group) {
 	c.Observable.SetState(snap)
 }
 
-func (c *core) Valid() bool {
-	return c.Observable.GetState().HostID != node.ID(0)
-}
+// Valid implements Store.
+func (c *core) Valid() bool { return c.Observable.GetState().HostID != node.ID(0) }
 
+// Load implements kv.FlushLoader.
 func (c *core) Load(r io.Reader) error {
 	var snap State
 	catch := errutil.NewCatchRead(r)
@@ -85,6 +104,7 @@ func (c *core) Load(r io.Reader) error {
 	return catch.Error()
 }
 
+// Flush implements kv.FlushLoader.
 func (c *core) Flush(w io.Writer) error {
 	catch := errutil.NewCatchWrite(w)
 	catch.Write(c.Observable.GetState())
---
+++ internal/cluster/store/store_test.go
@@ -0,0 +1,68 @@
+package store_test
+
+import (
+	"github.com/arya-analytics/aspen/internal/cluster/store"
+	"github.com/arya-analytics/aspen/internal/node"
+	"github.com/arya-analytics/x/version"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Store", func() {
+	var (
+		s store.Store
+	)
+	BeforeEach(func() {
+		s = store.New()
+	})
+	Describe("New", func() {
+		It("Should open a new store with empty state", func() {
+			Expect(s.GetState().Nodes).ToNot(BeNil())
+		})
+	})
+	Describe("Set and Get", func() {
+		It("Should set a node in store", func() {
+			s.Set(node.Node{ID: 1})
+			n, ok := s.Get(1)
+			Expect(ok).To(BeTrue())
+			Expect(n.ID).To(Equal(node.ID(1)))
+		})
+	})
+	Describe("Merge", func() {
+		It("Should add nonexistent nodes", func() {
+			s.Merge(node.Group{1: node.Node{ID: 1}})
+			n, ok := s.Get(1)
+			Expect(ok).To(BeTrue())
+			Expect(n.ID).To(Equal(node.ID(1)))
+		})
+		It("Should replaces nodes with an old heartbeat", func() {
+			s.Set(node.Node{ID: 1})
+			s.Merge(node.Group{1: node.Node{ID: 1, Heartbeat: version.Heartbeat{
+				Version:    1,
+				Generation: 0,
+			}}})
+			n, ok := s.Get(1)
+			Expect(ok).To(BeTrue())
+			Expect(n.ID).To(Equal(node.ID(1)))
+			Expect(n.Heartbeat.Version).To(Equal(uint32(1)))
+		})
+	})
+	Describe("Valid", func() {
+		It("Should return false if the host is not set", func() {
+			Expect(s.Valid()).To(BeFalse())
+		})
+		It("Should return true if the host is set", func() {
+			s.SetHost(node.Node{ID: 1})
+			Expect(s.Valid()).To(BeTrue())
+		})
+	})
+	Describe("GetHost", func() {
+		It("Should set and get the host correctly", func() {
+			s.SetHost(node.Node{ID: 1})
+			Expect(s.GetHost().ID).To(Equal(node.ID(1)))
+		})
+		It("Should panic when the host is not set", func() {
+			Expect(func() { s.GetHost() }).To(Panic())
+		})
+	})
+})
